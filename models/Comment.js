66cd50f9d743c8f8495a87bf42aa38bf844ae25f
Tank <johnson90.cm@gmail.com>
2021-11-22 14:20:16 -0700

    build 1.0

--- models/Comment.js
+++ models/Comment.js
@@ -7,19 +7,22 @@ const ReplySchema = new Schema(
         // set custom id to avoid confusion with parent comment _id
         replyId: {
             type: Schema.Types.ObjectId,
-            default: () => Types.ObjectId()
+            default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
         },
         createdAt: {
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
-        },
+        }
     },
     {
         toJSON: {
@@ -31,10 +34,13 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
     {
         writtenBy: {
-            type: String
+            type: String,
+            trim: true
         },
         commentBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createdAt: {
             type: Date,
@@ -52,9 +58,9 @@ const CommentSchema = new Schema(
     }
 );
 
-CommentSchema.virtual('replyCount').get(function() {
+CommentSchema.virtual('replyCount').get(function () {
     return this.replies.length;
-  });
+});
 
 const Comment = model('Comment', CommentSchema);
 
